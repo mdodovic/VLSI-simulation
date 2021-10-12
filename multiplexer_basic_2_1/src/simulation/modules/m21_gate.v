8448826acb89c8dd8897d3863eda589ce9ab7347
mdodovic <matija.dodovic@gmail.com>
2021-10-12 16:08:47 +0200

    add gate level multiplexer 21

--- multiplexer_basic_2_1/src/simulation/modules/m21_gate.v
+++ multiplexer_basic_2_1/src/simulation/modules/m21_gate.v
@@ -7,5 +7,12 @@ module m21_gate (
 
     input I0, I1, S0;
     output Y;
+
+    wire notS0, temp0, temp1;
+
+    not(notS0, S0);
+    and(temp0, I0, notS0);
+    and(temp1, I1, S0);
+    or(Y, temp0, temp1);
     
 endmodule
