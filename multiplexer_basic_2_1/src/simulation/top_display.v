d449bdee50ec13e6d4187c90ab9157d13012fe4c
mdodovic <matija.dodovic@gmail.com>
2021-10-12 19:47:36 +0200

    set display on out change

--- multiplexer_basic_2_1/src/simulation/top_display.v
+++ multiplexer_basic_2_1/src/simulation/top_display.v
@@ -9,20 +9,22 @@ module top_display;
         i0 = 1'b0;
         i1 = 1'b0;
         s0 = 1'b0;
-        $display("Time: %d, Out: %d", $time, out);
+        
         #10
         i0 = 1'b1;
-        $display("Time: %d, Out: %d", $time, out);
+        
         #10
         s0 = 1'b1;
-        $display("Time: %d, Out: %d", $time, out);
+        
         #10
         i1 = 1'b1;
-        $display("Time: %d, Out: %d", $time, out);
+        
         #10
         $stop;
     end
 
-
+    always @(out) begin
+        $display("Time: %d, Out: %d", $time, out);
+    end
 
 endmodule
