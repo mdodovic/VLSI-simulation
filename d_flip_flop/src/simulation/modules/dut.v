4fe1273d682464035ee81f891d9efc940a823440
mdodovic <matija.dodovic@gmail.com>
2021-10-21 09:53:14 +0200

    add dff logic

--- d_flip_flop/src/simulation/modules/dut.v
+++ d_flip_flop/src/simulation/modules/dut.v
@@ -1,3 +1,19 @@
-module dut;
+module dut (
+	clk, rst_n, d, q
+);
+
+	input clk, rst_n, d;
+	output reg q;
+
+	// clk:: on every change BUT on rising edge: posedge
+	// rst_n:: on every drop from 1 to 0: negedge
+	always @(posedge clk, negedge rst_n) begin
+
+		if (!rst_n)
+			q = 0;			
+		else
+			q = d;
+
+	end
 
 endmodule
