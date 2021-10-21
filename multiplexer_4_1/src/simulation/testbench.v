6544d490c2ce9f2301de84fe1246f9457256b0a0
mdodovic <matija.dodovic@gmail.com>
2021-10-21 09:20:59 +0200

    add every inputs' combinations testing

--- multiplexer_4_1/src/simulation/testbench.v
+++ multiplexer_4_1/src/simulation/testbench.v
@@ -1,10 +1,24 @@
 module testbench;
 
 	reg i0, i1, i2, i3;
-	reg [1:0] s;
+	reg s0, s1;
 	wire out;
+	integer index;
 
-	dut m41(i0, i1, i2, i3, s, out);
+	dut m41(i0, i1, i2, i3, {s1, s0}, out);
+
+	initial begin
+
+		for (index = 0; index < 64; index = index + 1) begin
+			{s1, s0, i3, i2, i1, i0} = index;		
+			#5;
+		end
+		
+		$finish;
+
+	end
+
+/*
 
 	initial begin
 
@@ -12,16 +26,18 @@ module testbench;
 		i1 = 1'b0;
 		i2 = 1'b0;
 		i3 = 1'b0;
-		s = 2'b00;
+		{s1, s0} = 2'b00;
+
 		#320 $finish;
+
 	end
 
 	always #5 i0 = ~i0; // after 5 units of time, change i0
 	always #10 i1 = ~i1; // after 10 units of time, change i1
 	always #20 i2 = ~i2; // after 20 units of time, change i2
 	always #40 i3 = ~i3; // after 40 units of time, change i3
-	always #80 s[0] = ~s[0]; // after 80 units of time, change s[0]
-	always #160 s[1] = ~s[1]; // after 160 units of time, change s[1]
-
+	always #80 s0 = ~s0; // after 80 units of time, change s[0]
+	always #160 s1 = ~s1; // after 160 units of time, change s[1]
+*/
 
 endmodule
--- multiplexer_4_1/tooling/config/waveform-define.do
+++ multiplexer_4_1/tooling/config/waveform-define.do
@@ -4,7 +4,8 @@ add wave -noupdate /testbench/i0
 add wave -noupdate /testbench/i1
 add wave -noupdate /testbench/i2
 add wave -noupdate /testbench/i3
-add wave -noupdate /testbench/s
+add wave -noupdate /testbench/s0
+add wave -noupdate /testbench/s1
 add wave -noupdate /testbench/out
 TreeUpdate [SetDefaultTree]
 WaveRestoreCursors {{Cursor 1} {0 ps} 0}
