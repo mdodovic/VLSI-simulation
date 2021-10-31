948c4ef22f9019fadcd79da31720c0827f18c15b
mdodovic <matija.dodovic@gmail.com>
2021-10-31 10:54:52 +0100

    add inc property with out not with out_next

--- register_8b/src/simulation/modules/dut.v
+++ register_8b/src/simulation/modules/dut.v
@@ -2,14 +2,20 @@ module dut(clk, rst_n, ld, inc, in, out);
 
 	input clk, rst_n, ld, inc;
 	input [7:0] in;
-	output reg [7:0] out;
+	output /*reg*/ [7:0] out;
+
+	reg [7:0] out_reg;
+	reg [7:0] out_next;
+	assign out = out_reg;
 
 	always @(posedge clk, negedge rst_n) begin
 		
 		if(!rst_n)
-			out <= 8'h00;
+			// out <= 8'h00;
+			out_reg <= 8'h00;
 		else 
 			// if both inc and ld singals are active, ld should be treated first
+			/*			
 			if (ld)
 				out <= in;
 			else 
@@ -21,6 +27,24 @@ module dut(clk, rst_n, ld, inc, in, out);
 										//  ^ and concate it with 1 will give => 00000001 
 
 				end
+			*/
+			out_reg <= out_next;
 	end
 	
+	always @(ld, inc, in, out) begin
+		// combinational block
+		// sensitivity list, first create logic, then see what has been used.
+
+		out_next = out_reg; // always it is good, to set out_next to value that is previous active value
+		// this is good because if-than-else structure may not assign out_next
+
+		if (ld)
+				out_next = in;
+		else 
+			if(inc)
+				out_next <= out + {{7{1'b0}}, 1'b1};
+			// in this else out_next will reamin same, so safety assign in the begining of the always block
+
+	end
+
 endmodule
