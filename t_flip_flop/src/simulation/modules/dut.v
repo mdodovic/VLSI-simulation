48f232f3772bfd2a12cfaeec9b7183f08b3f4c7e
mdodovic <matija.dodovic@gmail.com>
2021-11-16 15:50:17 +0100

    create T flip flop

--- t_flip_flop/src/simulation/modules/dut.v
+++ t_flip_flop/src/simulation/modules/dut.v
@@ -3,4 +3,22 @@ module dut(clk, rst_n, t, q);
 	input clk, rst_n, t;
 	output q;
 
+	reg q_reg, q_next;
+	assign q = q_reg;
+
+	always @(posedge clk, negedge rst_n) begin
+		
+		if(!rst_n)
+			q_reg <= 0;
+		else
+			q_reg <= q_next;	
+	end
+
+	always @(t) begin
+		if(t)
+			q_next = ~q_reg;
+		else
+			q_next = q_reg;
+	end
+
 endmodule
