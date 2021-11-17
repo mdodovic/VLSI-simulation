6e372ed5c1df0b92506eaf5b72c840d191ae6e18
mdodovic <matija.dodovic@gmail.com>
2021-11-17 09:24:46 +0100

    create testbench

--- parity_counter_and_shifter/solution_parity/src/simulation/stavka_b.v
+++ parity_counter_and_shifter/solution_parity/src/simulation/stavka_b.v
@@ -1,21 +1,26 @@
 module stavka_b;
 
-	reg i0, i1, s0;
-	wire out;
+	reg [6:0] data_in;
+	reg control;
+	wire [7:0] data_out;
 
-	stavka_a m21(.I0(i0), .I1(i1), .S0(s0), .Y(out));
+	stavka_a parity_bit_counter(data_in, control, data_out);
+
+	integer i;
 
 	initial begin
-		i0 = 1'b0;
-		i1 = 1'b0;
-		s0 = 1'b0;
-		#10 i0 = 1'b1;
-		#10 s0 = 1'b1;
-		#10 i1 = 1'b1;
-		#10 $stop;
+		data_in = 7'h00; 
+		control = 1'b0;
+		
+		for (i = 0; i < 256; i = i + 1) begin
+			{ data_in, control }= i;
+			#5;			
+		end
+		$finish;
 	end
 
-	always @(out)
-		$display("Vreme = %0d, Izlaz = %d", $time, out); 
-
+	always @(data_out) begin
+		$display("time = %4d, dut_data_in = %b, dut_control = %b, dut_data_out = %b", $time, data_in, control, data_out);
+	end
+	
 endmodule
