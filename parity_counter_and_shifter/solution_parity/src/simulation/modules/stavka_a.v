e634f2bbbaa400f582c5bef84507ff2a014bf159
mdodovic <matija.dodovic@gmail.com>
2021-11-17 09:24:52 +0100

    create dut

--- parity_counter_and_shifter/solution_parity/src/simulation/modules/stavka_a.v
+++ parity_counter_and_shifter/solution_parity/src/simulation/modules/stavka_a.v
@@ -1,13 +1,44 @@
-module stavka_a(I0, I1, S0, Y);
+module stavka_a (
+	data_in, control, data_out
+);
+	
+	input [6:0] data_in;
+	input control;
 
-	input I0, I1, S0;
-	output Y;
+	output reg [7:0] data_out;
 
-	wire notS0, T0, T1;
+	integer zero_counter;
+	integer one_counter;
 
-	not (notS0, S0);
-	and (T0, I1, S0);
-	and (T1, I0, notS0);
-	or (Y, T0, T1);
+	integer i;
+
+	reg desired_output_bit;
+
+	always @(*) begin
+		$display(data_in, control);		
+		for (i = 0; i < 6 ; i = i + 1) begin
+			if(data_in[i] == 1'b0) 
+				zero_counter = zero_counter + 1;
+			else
+				one_counter = one_counter + 1;
+		end
+
+		if(control == 1'b0) begin
+			if(zero_counter > one_counter)
+				desired_output_bit = 1'b1;
+			else 
+				desired_output_bit = 1'b0;
+		end
+		else begin
+			if(one_counter > zero_counter)
+				desired_output_bit = 1'b1;
+			else 
+				desired_output_bit = 1'b0;
+		end
+
+		data_out = {data_in[6], data_in[5], data_in[4], data_in[3], desired_output_bit, data_in[2], data_in[1], data_in[0]};
+
+	end
+
 
 endmodule
